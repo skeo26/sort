63e72b427aa7bb635b29371baab6e38bcf907159
Semyon Koltygin <koltyginsemyon@gmail.com>
2023-10-27 11:38:00 +0700

    Update

---
+++ sort/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace VsualSorfWF
+{
+	partial class Sort
+	{
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LabelTime = new System.Windows.Forms.Label();
+            this.maxLim = new System.Windows.Forms.NumericUpDown();
+            this.minLim = new System.Windows.Forms.NumericUpDown();
+            this.GnomeSort = new System.Windows.Forms.Button();
+            this.ShellSort = new System.Windows.Forms.Button();
+            this.InsertionSort = new System.Windows.Forms.Button();
+            this.ShakerSort = new System.Windows.Forms.Button();
+            this.countElements = new System.Windows.Forms.NumericUpDown();
+            this.SortVibor = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxLim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minLim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countElements)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.LabelTime);
+            this.groupBox1.Controls.Add(this.maxLim);
+            this.groupBox1.Controls.Add(this.minLim);
+            this.groupBox1.Controls.Add(this.GnomeSort);
+            this.groupBox1.Controls.Add(this.ShellSort);
+            this.groupBox1.Controls.Add(this.InsertionSort);
+            this.groupBox1.Controls.Add(this.ShakerSort);
+            this.groupBox1.Controls.Add(this.countElements);
+            this.groupBox1.Controls.Add(this.SortVibor);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 411);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1301, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки";
+            // 
+            // LabelTime
+            // 
+            this.LabelTime.AutoSize = true;
+            this.LabelTime.Location = new System.Drawing.Point(521, 43);
+            this.LabelTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LabelTime.Name = "LabelTime";
+            this.LabelTime.Size = new System.Drawing.Size(0, 16);
+            this.LabelTime.TabIndex = 18;
+            // 
+            // maxLim
+            // 
+            this.maxLim.Location = new System.Drawing.Point(310, 45);
+            this.maxLim.Margin = new System.Windows.Forms.Padding(4);
+            this.maxLim.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.maxLim.Name = "maxLim";
+            this.maxLim.Size = new System.Drawing.Size(132, 22);
+            this.maxLim.TabIndex = 17;
+            this.maxLim.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // minLim
+            // 
+            this.minLim.Location = new System.Drawing.Point(162, 43);
+            this.minLim.Margin = new System.Windows.Forms.Padding(4);
+            this.minLim.Name = "minLim";
+            this.minLim.Size = new System.Drawing.Size(132, 22);
+            this.minLim.TabIndex = 16;
+            this.minLim.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // GnomeSort
+            // 
+            this.GnomeSort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.GnomeSort.Location = new System.Drawing.Point(1139, 24);
+            this.GnomeSort.Margin = new System.Windows.Forms.Padding(4);
+            this.GnomeSort.Name = "GnomeSort";
+            this.GnomeSort.Size = new System.Drawing.Size(99, 43);
+            this.GnomeSort.TabIndex = 13;
+            this.GnomeSort.Text = "Gnome";
+            this.GnomeSort.UseVisualStyleBackColor = true;
+            this.GnomeSort.Click += new System.EventHandler(this.GnomeSort_Click);
+            // 
+            // ShellSort
+            // 
+            this.ShellSort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShellSort.Location = new System.Drawing.Point(1019, 23);
+            this.ShellSort.Margin = new System.Windows.Forms.Padding(4);
+            this.ShellSort.Name = "ShellSort";
+            this.ShellSort.Size = new System.Drawing.Size(99, 43);
+            this.ShellSort.TabIndex = 12;
+            this.ShellSort.Text = "Shell";
+            this.ShellSort.UseVisualStyleBackColor = true;
+            this.ShellSort.Click += new System.EventHandler(this.ShellSort_Click);
+            // 
+            // InsertionSort
+            // 
+            this.InsertionSort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.InsertionSort.Location = new System.Drawing.Point(894, 23);
+            this.InsertionSort.Margin = new System.Windows.Forms.Padding(4);
+            this.InsertionSort.Name = "InsertionSort";
+            this.InsertionSort.Size = new System.Drawing.Size(99, 43);
+            this.InsertionSort.TabIndex = 11;
+            this.InsertionSort.Text = "Insertion";
+            this.InsertionSort.UseVisualStyleBackColor = true;
+            this.InsertionSort.Click += new System.EventHandler(this.InsertionSort_Click);
+            // 
+            // ShakerSort
+            // 
+            this.ShakerSort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShakerSort.Location = new System.Drawing.Point(771, 23);
+            this.ShakerSort.Margin = new System.Windows.Forms.Padding(4);
+            this.ShakerSort.Name = "ShakerSort";
+            this.ShakerSort.Size = new System.Drawing.Size(103, 42);
+            this.ShakerSort.TabIndex = 10;
+            this.ShakerSort.Text = "Shaker";
+            this.ShakerSort.UseVisualStyleBackColor = true;
+            this.ShakerSort.Click += new System.EventHandler(this.ShakerSort_Click);
+            // 
+            // countElements
+            // 
+            this.countElements.Location = new System.Drawing.Point(8, 43);
+            this.countElements.Margin = new System.Windows.Forms.Padding(4);
+            this.countElements.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.countElements.Name = "countElements";
+            this.countElements.Size = new System.Drawing.Size(132, 22);
+            this.countElements.TabIndex = 7;
+            this.countElements.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // SortVibor
+            // 
+            this.SortVibor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SortVibor.Location = new System.Drawing.Point(656, 23);
+            this.SortVibor.Margin = new System.Windows.Forms.Padding(4);
+            this.SortVibor.Name = "SortVibor";
+            this.SortVibor.Size = new System.Drawing.Size(96, 42);
+            this.SortVibor.TabIndex = 6;
+            this.SortVibor.Text = "Viborom";
+            this.SortVibor.UseVisualStyleBackColor = true;
+            this.SortVibor.Click += new System.EventHandler(this.SortVibor_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(307, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Верхняя граница";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Нижняя граница";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кол-во элементов ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(16, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(1301, 389);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Отображение сортировок";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1285, 358);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(521, 23);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Время";
+            // 
+            // Sort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1333, 501);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(669, 410);
+            this.Name = "Sort";
+            this.Text = "Sort";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxLim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minLim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countElements)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button SortVibor;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.NumericUpDown countElements;
+        private System.Windows.Forms.Button ShakerSort;
+        private System.Windows.Forms.Button InsertionSort;
+        private System.Windows.Forms.Button ShellSort;
+        private System.Windows.Forms.Button GnomeSort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown maxLim;
+        private System.Windows.Forms.NumericUpDown minLim;
+        private System.Windows.Forms.Label LabelTime;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ sort/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace VsualSorfWF
+{
+	public partial class Sort : Form 
+	{
+		public int[] array;
+
+		public Sort()
+		{
+			InitializeComponent();
+		}
+
+		private void SortVibor_Click(object sender, EventArgs e)
+		{
+			DateTime start = DateTime.Now;
+			Random random = new Random();
+			array = new int[(int)countElements.Value];
+			array = array.Select(x => random.Next((int)minLim.Value, (int)(maxLim.Value + 1))).ToArray();
+			for (int i = 0; i < array.Length; i++)
+			{
+				int min = i;
+				for (int j = i + 1; j < array.Length; j++)
+					if (array[j] < array[min])
+						min = j;
+
+				if (min != i)
+					swap(ref array[i], ref array[min]);
+
+				drawSort(array);
+				//drawMarking();
+				Thread.Sleep(100);
+			}
+			DateTime end = DateTime.Now;
+			TimeSpan ts = (end - start);
+			LabelTime.Text = Convert.ToString(ts.Milliseconds);
+		}
+		private void ShakerSort_Click(object sender, EventArgs e)
+		{
+			DateTime start = DateTime.Now;
+			Random random = new Random();
+			array = new int[(int)countElements.Value];
+			array = array.Select(x => random.Next((int)minLim.Value, (int)(maxLim.Value + 1))).ToArray();
+			for (var i = 0; i < array.Length / 2; i++)
+			{
+				var swapFlag = false;
+				//проход слева направо
+				for (var j = i; j < array.Length - i - 1; j++)
+				{
+					if (array[j] > array[j + 1])
+					{
+						Swap(ref array[j], ref array[j + 1]);
+						swapFlag = true;
+					}
+				}
+
+				//проход справа налево
+				for (var j = array.Length - 2 - i; j > i; j--)
+				{
+					if (array[j - 1] > array[j])
+					{
+						Swap(ref array[j - 1], ref array[j]);
+						swapFlag = true;
+					}
+				}
+
+				//если обменов не было выходим
+				if (!swapFlag)
+					break;
+				drawSort(array);
+				//drawMarking();
+				Thread.Sleep(1000);
+			}
+			DateTime end = DateTime.Now;
+			TimeSpan ts = (end - start);
+			LabelTime.Text = Convert.ToString(ts.Milliseconds);
+		}
+		private void InsertionSort_Click(object sender, EventArgs e)
+		{
+			DateTime start = DateTime.Now;
+			Random random = new Random();
+			array = new int[(int)countElements.Value];
+			array = array.Select(x => random.Next((int)minLim.Value, (int)(maxLim.Value + 1))).ToArray();
+			for (int i = 1; i < array.Length; i++)
+			{
+				int cur = array[i];
+				int j = i;
+				while (j > 0 && cur < array[j - 1])
+				{
+					array[j] = array[j - 1];
+					j--;
+				}
+				array[j] = cur;
+			
+				drawSort(array);
+				//drawMarking();
+				Thread.Sleep(100);
+			}
+			DateTime end = DateTime.Now;
+			TimeSpan ts = (end - start);
+			LabelTime.Text = Convert.ToString(ts.Milliseconds);
+
+		}
+		private void ShellSort_Click(object sender, EventArgs e)
+		{
+			DateTime start = DateTime.Now;
+			Random random = new Random();
+			array = new int[(int)countElements.Value];
+			array = array.Select(x => random.Next((int)minLim.Value, (int)(maxLim.Value + 1))).ToArray();
+			var step = array.Length / 2; // шаг
+			while (step >= 1)
+			{
+				for (var i = step; i < array.Length; i++)
+				{
+					var j = i;
+					while ((j >= step) && (array[j - step] > array[j]))
+					{
+						Swap(ref array[j], ref array[j - step]);
+						j = j - step;
+					}
+				}
+
+				step = step / 2;
+				drawSort(array);
+				//drawMarking();
+				Thread.Sleep(100);
+
+			}
+			DateTime end = DateTime.Now;
+			TimeSpan ts = (end - start);
+			LabelTime.Text = Convert.ToString(ts.Milliseconds);
+		}
+
+
+		private void swap<T>(ref T a, ref T b)
+		{
+			T c = a;
+			a = b;
+			b = c;
+		}
+		static void Swap(ref int e1, ref int e2)
+		{
+			var temp = e1;
+			e1 = e2;
+			e2 = temp;
+		}
+
+
+		private void drawSort(int[] array)
+		{
+			bool flag = true;
+			Pen pen = new Pen(Color.DarkViolet);
+
+
+			Graphics graphics = pictureBox1.CreateGraphics();
+			graphics.Clear(Color.DarkGray);
+			for (int i = (int)minLim.Value; i <= maxLim.Value; i++)
+			{
+				for (int j = 0; j < array.Length; j++)
+				{
+					if (flag)
+						pen = new Pen(Color.DarkKhaki);
+					else
+						pen = new Pen(Color.LightYellow);
+					flag = !flag;
+
+					if (array[j] >= i)
+						graphics.FillRectangle(new SolidBrush(pen.Color), 10 * j, pictureBox1.Height - 10 * i, 10, 10);
+				}
+			}
+		}
+
+        //private void drawMarking()
+        //{
+        //    Graphics graphics = pictureBox1.CreateGraphics();
+
+        //    Pen pen = new Pen(Color.Black);
+        //    for (int i = 0; i < pictureBox1.Height; i += 8)
+        //        graphics.DrawLine(pen, 0, pictureBox1.Height - i, pictureBox1.Width, pictureBox1.Height - i);
+        //    for (int i = 0; i < pictureBox1.Width; i += 8)
+        //        graphics.DrawLine(pen, i, 0, i, pictureBox1.Width);
+        //}
+
+  //      private void pictureBox1_Paint(object sender, PaintEventArgs e)
+		//{
+		//	((PictureBox)sender).CreateGraphics().Clear(Color.Black);
+		//	drawMarking();
+		//}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+        private void GnomeSort_Click(object sender, EventArgs e)
+        {
+			DateTime start = DateTime.Now;
+			Random random = new Random();
+				array = new int[(int)countElements.Value];
+			array = array.Select(x => random.Next((int)minLim.Value, (int)(maxLim.Value + 1))).ToArray();
+			var index = 1;
+				var nextIndex = index + 1;
+
+				while (index < array.Length)
+				{
+					if (array[index - 1] < array[index])
+					{
+						index = nextIndex;
+						nextIndex++;
+					}
+					else
+					{
+						Swap(ref array[index - 1], ref array[index]);
+						index--;
+						if (index == 0)
+						{
+							index = nextIndex;
+							nextIndex++;
+						}
+					}
+				drawSort(array);
+				//drawMarking();
+				Thread.Sleep(100);
+			}
+			DateTime end = DateTime.Now;
+			TimeSpan ts = (end - start);
+			LabelTime.Text = Convert.ToString(ts.Milliseconds);
+		}
+
+        private void minLim_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+    }
+
+
+
